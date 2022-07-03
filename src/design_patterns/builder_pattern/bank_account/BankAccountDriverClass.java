c5268a5372cc364e4872e71e46befb717774a4d6
Mehedi32HSTU <mehedi32.cse.hstu@gmail.com>
2022-07-03 18:07:29 +0600

    Builder pattern for bank account modified with setter name.

--- src/design_patterns/builder_pattern/bank_account/BankAccountDriverClass.java
+++ src/design_patterns/builder_pattern/bank_account/BankAccountDriverClass.java
@@ -3,18 +3,18 @@ package design_patterns.builder_pattern.bank_account;
 public class BankAccountDriverClass {
 	public static void main(String[] args) {
 		BankAccount bankAccountForMehedi = new BankAccount.Builder(12345L)
-				.withOwner("Mehedi Hasan")
-				.atBranch("Uttara")
-				.openingBalance(50000.0)
-				.atRate(7.5)
+				.setOwnerName("Mehedi Hasan")
+				.setBranchName("Uttara")
+				.setBalance(50000.0)
+				.setInterestRate(7.5)
 				.build();
+
 		BankAccount bankAccountForAngshu = new BankAccount.Builder(1234L)
-				.withOwner("Angshuman")
-				.atBranch("Uttara")
-				.openingBalance(50000.0)
+				.setOwnerName("Angshuman")
+				.setBranchName("Uttara-3")
+				.setBalance(55000.0)
 				.build();
 		System.out.println("Mehedi Account Details : "+ bankAccountForMehedi.toString());
 		System.out.println("Angshuman Account Details : "+ bankAccountForAngshu.toString());
 	}
-
 }
